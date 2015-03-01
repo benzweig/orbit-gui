721323a012d401386e452c70c252c9438cda66c8
Ben Zweig <benzilla@me.com>
2015-03-01 03:14:10 -0800

    Add icon hover events and runCommand

---
+++ Popup/PanelButton.h
@@ -0,0 +1,13 @@
+//
+//  PanelImageView.h
+//  Popup
+//
+//  Created by Katherine Fang on 2/28/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PanelButton : NSButton
+@property (nonatomic, strong) NSTrackingArea* trackingArea;
+@end
---
+++ Popup/PanelButton.m
@@ -0,0 +1,72 @@
+//
+//  PanelImageView.m
+//  Popup
+//
+//  Created by Katherine Fang on 2/28/15.
+//
+//
+
+#import "PanelButton.h"
+
+
+@implementation PanelButton
+// Monkey patch from http://stackoverflow.com/a/12310154
+NSString *runCommand(NSString *commandToRun)
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/bin/sh"];
+    
+    NSArray *arguments = [NSArray arrayWithObjects:
+                          @"-c" ,
+                          [NSString stringWithFormat:@"%@", commandToRun],
+                          nil];
+    NSLog(@"run command: %@",commandToRun);
+    [task setArguments: arguments];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
+    [task launch];
+    
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *output;
+    output = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    return output;
+}
+
+
+//Add this to Your imageView subclass
+
+-(void)mouseEntered:(NSEvent *)theEvent {
+    
+    NSLog(@"Mouse entered %@", self.identifier);
+    NSString *output = runCommand(@"say it worked");
+}
+
+-(void)mouseExited:(NSEvent *)theEvent
+{
+    NSLog(@"Mouse exited");
+}
+
+-(void)updateTrackingAreas
+{
+    if(self.trackingArea != nil) {
+        [self removeTrackingArea:self.trackingArea];
+    }
+    
+    int opts = (NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways);
+    self.trackingArea = [ [NSTrackingArea alloc] initWithRect:[self bounds]
+                                                 options:opts
+                                                   owner:self
+                                                userInfo:nil];
+    [self addTrackingArea:self.trackingArea];
+}
+
+@end
